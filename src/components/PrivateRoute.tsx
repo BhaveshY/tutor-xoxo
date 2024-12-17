de03a7d2dd50c0bf8720cfa34a4da2c9dae43ae5
bhaveshY <byeole80@gmail.com>
2024-12-17 21:07:27 +0100

    fixed minor issues

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,6 +1,8 @@
 import { Navigate } from 'react-router-dom';
 import useStore from '../../tutorgpt/src/store/useStore.ts';
 
+
+
 import React from 'react';
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
